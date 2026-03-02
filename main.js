c601a645734a27281066b65b0222802693f0e3ac
slexedward191 <slexedward191@gmail.com>
2026-03-02 09:15:48 +0300

    Add functions to toggle home and status pages

---
+++ main.js
@@ -0,0 +1,9 @@
+function showStatus(){
+    document.getElementById("homePage").style.display="none";
+    document.getElementById("statusPage").style.display="block";
+}
+
+function showHome(){
+    document.getElementById("homePage").style.display="block";
+    document.getElementById("statusPage").style.display="none";
+}
